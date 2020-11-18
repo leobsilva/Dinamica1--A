675516a0012ca9bd380777c40e0eeb8c45245749
leobsilva <31291693+leobsilva@users.noreply.github.com>
2020-11-18 14:40:56 -0300

    Create dataSaver.java

---
+++ dataSaver.java
@@ -0,0 +1,60 @@
+class dataSaver {
+  private int dia;
+  private int mes;
+  private int ano;
+  private String ampm;
+  private int hora;
+  private int minutos;
+
+  public void setDia(int dia) {
+    this.dia = dia;
+  }
+
+  public int getDia() {
+    return this.dia;
+  }
+
+  public void setMes(int mes) {
+    this.mes = mes;
+  }
+
+  public int getMes() {
+    return this.mes;
+  }
+
+  public void setAno(int ano) {
+    this.ano = ano;
+  }
+
+  public int getAno() {
+    return this.ano;
+  }
+
+  public void setHora(int hora) {
+    this.hora = hora;
+  }
+
+  public int getHora() {
+    return this.hora;
+  }
+
+  public void setMinuto(int minuto) {
+    this.minuto = minuto;
+  }
+
+  public int getMinuto() {
+    return this.minuto;
+  }
+
+  public void setAmpm(String ampm) {
+    this.ampm = ampm;
+  }
+
+  public String getAmpm() {
+    return this.ampm;
+  }
+}
+class Main {
+  public static void main(String[] args) {
+  }
+}
